f83e110de1a156a8ab17304c0e13500ceeb41de7
Toghrul-Mehdi <togrulcm-bp215@code.edu.az>
2024-10-15 01:34:10 +0400

    Class

---
+++ AvtoSalon/AvtoSalon/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace AvtoSalon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Opel","Astra",5000,4);
+            car1.ShowFullInfo();
+            Car car2 = new Car("Honda", "Insight", 7000, 4);
+            car2.ShowFullInfo();
+            Car car3 = new Car("Hyundai", "Santafe", 8000, 4);
+            car3.ShowFullInfo();
+
+            Motorcycle moto1 = new Motorcycle("Ducati","Multistrada",3000,2);
+            moto1.ShowFullInfo();
+            Motorcycle moto2 = new Motorcycle("Ducati", "Scrambler", 4000, 2);
+            moto2.ShowFullInfo();
+            Motorcycle moto3 = new Motorcycle("Ducati", "Panigale", 2000, 2);
+            moto3.ShowFullInfo();
+        }
+    class Vehicle
+        {
+            public string Brand;
+            public string Model;
+            public double Mile;
+            public Vehicle(string brand ,string model, double mile) 
+            {
+                if(string.IsNullOrEmpty(model) || string.IsNullOrEmpty(brand))
+                {
+                    throw new ArgumentNullException("Marka ve model mutleq qeyd olunmalidir!");
+                }
+                Brand = brand;
+                Model = model;
+                Mile = mile;
+            }
+            public void ShowFullInfo()
+            {
+                Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile}");
+            }
+        }
+    class Car : Vehicle
+        {
+            public int DoorsNum;
+            public Car(string brand, string model, double mile, int doorsNum) : base(brand, model, mile)
+            {
+                DoorsNum = doorsNum;
+            }
+
+            public void ShowFullInfo()
+            {
+                base.ShowFullInfo();
+                Console.WriteLine($"Qapi sayi: {DoorsNum}");
+            }
+        }
+    class Motorcycle:Vehicle
+        {
+            public int WheelNum;
+
+            public Motorcycle(string brand, string model, double mile, int wheelNum):base(brand, model, mile)
+            {
+                WheelNum = wheelNum;
+            }
+
+            public void ShowFullInfo()
+            {
+                base.ShowFullInfo();
+                Console.WriteLine($"Teker sayi: {WheelNum}");
+            }
+        }
+    
+    }
+}
---
+++ CompStore/CompStore/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace CompStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook Acer = new Notebook("Acer", "Yaxsi kompdu", 3, 2500, 16, 512);
+            Acer.ShowFullData();
+            Notebook Hp = new Notebook("Hp", "Yaxsi kompdu", 4, 1500, 8, 256);
+            Hp.ShowFullData();
+            Notebook Lenovo = new Notebook("Lenovo", "Yaxsi kompdu", 0, 1500, 8, 512);
+            Lenovo.ShowFullData();
+        }
+    class Product
+        {
+            public string Name;
+            public string Description;
+            public int Count;
+            public bool IsStock = true;
+            public double Price;
+
+            public Product(string name, string description, int count, double price)
+            {
+                Name = name;
+                Description = description;
+                Count = count;
+                Price = price;
+            }
+
+            public void Sale(string name, string description, int count, double price)
+            {
+                
+                Name = name;
+                Description = description;
+                Count = count;
+                Price = price;
+            }
+            public void ShowFullData()
+            {
+                Console.WriteLine($"Name:{Name},Description:{Description},Price:{Price},Count:{Count},Price:{Price}");
+            }
+
+        }
+        class Notebook : Product
+        {
+            public byte Ram;
+            public int Storage;
+            public Notebook(string name, string description, int count, double price, byte ram, int storage) : base(name, description, count, price)
+            {
+                if (string.IsNullOrEmpty(name)|| count==' ' || price==' ' ||  ram==' ')
+                {
+                    throw new ArgumentException("Verilenler daxil edilmeyib");
+                }
+                else if (count==0)
+                {
+                    throw new ArgumentException("Mehsul stokda yoxdu");
+                }
+                Ram = ram;
+                Storage = storage;
+
+            }
+            public void ShowFullData()
+            {
+                base.ShowFullData();
+                Console.WriteLine($"Ram:{Ram},Storage:{Storage}");
+            }
+
+        }
+    }
+}
